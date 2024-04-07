e0c187a4e1f3a010af4b90da3b4db5428efca470
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2024-04-07 07:30:50 +0530

    Update core.py

--- TelethonHell/plugins/core.py
+++ TelethonHell/plugins/core.py
@@ -40,7 +40,7 @@ async def send(event):
     message_id = event.reply_to_msg_id or event.message.id
     thumb = hell_logo
     input_str = event.pattern_match.group(1)
-    omk = f"**• Plugin name ≈** `{input_str}`\n**• Uploaded by ≈** {hell_mention}\n\n⚡ **[ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt]({chnl_link})** ⚡"
+    omk = f"**• Plugin name ≈** `{input_str}`\n**• Uploaded by ≈** {hell_mention}\n\n⚡ **[𝐓𝐞𝐜𝐡𝐧𝐨 𝐛𝐨𝐭 𝐨𝐩](https://t.me/hellbot_plugins)** ⚡"
     the_plugin_file = "./TelethonHell/plugins/{}.py".format(input_str.lower())
     if os.path.exists(the_plugin_file):
         await event.client.send_file(
@@ -104,7 +104,7 @@ async def install(event):
                             b = 1
                         await hell.edit(a)
                     return await hell.edit(
-                        f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {hell_mention}\n\n{string}\n\n        ⚡ **[ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt]({chnl_link})** ⚡",
+                        f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {hell_mention}\n\n{string}\n\n        ⚡ **[𝐓𝐞𝐜𝐡𝐧𝐨 𝐛𝐨𝐭 𝐨𝐩](https://t.me/hellbot_plugins)** ⚡",
                         link_preview=False,
                     )
                 return await hell.edit(
